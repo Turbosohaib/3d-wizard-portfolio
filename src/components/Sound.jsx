d641a65d2b157c1ae3caa80d7e9f62167673afec
Sohaib Shah <turbosohaib1234@gmail.com>
2025-03-10 10:53:26 +0500

    Implementing audio component

--- src/components/Sound.jsx
+++ src/components/Sound.jsx
@@ -1,15 +1,44 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Volume2, VolumeX } from "lucide-react";
 import { motion } from "framer-motion";
 
 const Sound = () => {
+  const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
-  const toggle = () => setIsPlaying(!isPlaying);
+  const handleFirstUserInteraction = () => {
+    const musicConsent = localStorage.getItem("musicConsent");
+    if (musicConsent === "true" && !isPlaying) {
+      audioRef.current.play();
+      setIsPlaying(true);
+    }
+    ["click", "keydown", "touchstart"].forEach((event) =>
+      document.removeEventListener(event, handleFirstUserInteraction),
+    );
+  };
+
+  useEffect(() => {
+    const consent = localStorage.getItem("musicConsent");
+    if (consent) {
+      setIsPlaying(consent === "true");
+      if (consent === "true") {
+        ["click", "keydown", "touchstart"].forEach((event) =>
+          window.addEventListener(event, handleFirstUserInteraction),
+        );
+      }
+    }
+  }, []);
+
+  const toggle = () => {
+    const newState = !isPlaying;
+    setIsPlaying(newState);
+    newState ? audioRef.current.play() : audioRef.current.pause();
+    localStorage.setItem("musicConsent", String(newState));
+  };
   return (
     <div className="fixed top-4 right-2.5 xs:right-4 z-50">
-      <audio loop>
+      <audio ref={audioRef} loop>
         <source src={"/audio/birds39-forest-20772.mp3"} type="audio/mpeg" />
         your browser does not support the audio element.
       </audio>
@@ -18,7 +47,7 @@ const Sound = () => {
         initial={{ scale: 0 }}
         animate={{ scale: 1 }}
         transition={{ delay: 1 }}
-        className="text-foreground rounded-full flex items-center justify-center custom-bg fixed top-4 right-4 w-fit self-start z-50"
+        className="w-10 h-10 xs:w-14 xs:h-14 text-foreground rounded-full flex items-center justify-center cursor-pointer z-50 p-2.5 xs:p-4 custom-bg"
         aria-label={"home"}
         name={"home"}
       >
